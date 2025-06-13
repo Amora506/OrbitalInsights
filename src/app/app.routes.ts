27d8acf338c70b8999f5ea46bcdc316da7647d08
Alejandro Mora <moraa471@gmail.com>
2025-06-13 14:52:16 -0600

    chore: remove unncesary and add requiered imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { Home } from './layout/home/home/home';
+import { Galery } from './layout/galery/galery/galery';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: 'image-gallery',
+        component: Galery,
+    },
+    // {
+    //     path: 'explorer',
+    //     component: NodeThree,
+    // },
+    // {
+    //     path: 'calendar',
+    //     component: NodeFour,
+    // },
+    // {
+    //     path: 'simulator',
+    //     component: NodeFive,
+    // },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    MatSidenavModule,
+    RouterModule,
     MatButtonModule
-  ],
+    
+],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'OrbitalInsights';
+  
 }
