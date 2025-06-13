7928c331130d67ebd9310d1aa96340f2516984f2
Alejandro Mora <moraa471@gmail.com>
2025-06-13 14:50:32 -0600

    feat: add get Apod functionality

--- src/app/core/services/nasa-service.ts
+++ src/app/core/services/nasa-service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpResourceRef, httpResource } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IApod } from '../interfaces/IApod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NasaService {
+  private readonly apiKey = 'r6B3Bv8MELnFXnV8WnPvwZ71PcSAjqArSYhFjAhp';
+
+
+  http = inject(HttpClient);
+
+
+  getApod():HttpResourceRef<IApod | undefined> {
+    return  httpResource<IApod | undefined>(()=> `https://api.nasa.gov/planetary/apod?api_key=${this.apiKey}`);
+  }
 
   constructor() { }
 }
